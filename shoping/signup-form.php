323218051219e95385b6faf6adcb299f9d765add
Renan Angeles <Rennan.angeles22@gmail.com>
2022-11-23 13:35:57 +0900

    signup

---
+++ shoping/signup-form.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+
+	<title>Sign Up</title>
+</head>
+	<body>
+		<div class="container" style="margin-top: 20px">
+			<div class="card" style="width: 400px; margin-left: 400px;">
+			<article class="card-body mx-auto" style="max-width: 400px;">
+				<h4 class="card-title mt-3 text-center">Create Account</h4>
+				<p class="text-center">Get started with your free account</p>
+			
+				<form method="post">
+				<div class="form-group input-group">
+					<div class="input-group-prepend">
+					    <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+					 </div>
+			        <input name="" class="form-control" placeholder="Full name" type="text">
+			    </div>
+			    <div class="form-group input-group">
+			    	<div class="input-group-prepend">
+					    <span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
+					 </div>
+			        <input name="" class="form-control" placeholder="Email address" type="email">
+			    </div> 
+			    <div class="form-group input-group">
+			    	<div class="input-group-prepend">
+					    <span class="input-group-text"> <i class="fa fa-phone"></i> </span>
+					</div>
+			    	<input name="" class="form-control" placeholder="Phone number" type="text">
+			    </div> 
+			    <div class="form-group input-group">
+			    	<div class="input-group-prepend">
+					    <span class="input-group-text"> <i class="fa fa-building"></i> </span>
+					</div>
+					<select class="form-control">
+						<option selected=""> Select job type</option>
+						<option>Designer</option>
+						<option>Manager</option>
+						<option>Accaunting</option>
+					</select>
+				</div>
+			    <div class="form-group input-group">
+			    	<div class="input-group-prepend">
+					    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+					</div>
+			        <input class="form-control" placeholder="Create password" type="password">
+			    </div>
+			    <div class="form-group input-group">
+			    	<div class="input-group-prepend">
+					    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+					</div>
+			        <input class="form-control" placeholder="Repeat password" type="password">
+			    </div>                                    
+			    <div class="form-group">
+			        <button type="submit" class="btn btn-success btn-block"> Create Account  </button>
+			    </div>  
+			    <p class="text-center">Have an account? <a href="login_form.php">Log In</a> </p>                                                                 
+			</form>
+			</article>
+			</div>
+		</div> 
+		<br><br>
+		<article class="bg-secondary mb-3">  
+		<div class="card-body text-center">
+		    <h3 class="text-white mt-3">Try & try</h3>
+		<p class="h5 text-white">Auto Shift!</p><br>
+		</div>
+		<br><br>
+		</article>
+
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	</body>
+</html>
